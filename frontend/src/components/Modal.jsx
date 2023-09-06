384a290cf4e086be9a8071c84e867b4fcaf36bc5
pc <pc@pop-os.localdomain>
2023-09-06 03:26:01 +0300

    Updated Modal.jsx

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({ message, onConfirm, onCancel }) {
+function Modal({ message, onClose, onConfirm }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div className="bg-white p-4 rounded-md shadow-md">
@@ -8,7 +8,7 @@ function Modal({ message, onConfirm, onCancel }) {
         <div className="flex justify-end">
           <button
             className="mr-2 px-4 py-2 bg-gray-400 rounded text-white hover:bg-gray-500"
-            onClick={onCancel}
+            onClick={onClose}
           >
             Cancel
           </button>
