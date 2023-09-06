f7e800c9081707047987ea92738d7220d88b3783
pc <pc@pop-os.localdomain>
2023-09-06 03:29:24 +0300

    Added downloadDockerfile service

--- frontend/src/services/CreateDocker.js
+++ frontend/src/services/CreateDocker.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseUrl: "http://localhost:3001",
+  baseURL: "http://localhost:3001",
 });
 
-const createDockerfile = async (from, workdir, run, copy, run, cmd, expose) => {
+const createDockerfile = async (from, workdir, copy, run, cmd, expose) => {
     try {
-        const response = await api.post("/docker/create-dockerfile", {
+        const response = await api.post("/file/dockerfile", {
             from: from,
             workdir: workdir,
             copy: copy,
@@ -14,14 +14,41 @@ const createDockerfile = async (from, workdir, run, copy, run, cmd, expose) => {
             cmd: cmd,
             expose: expose
         });
-        console.log(response);
+        console.log(response.data);
     } catch (error) {
-        console("service Error: ", error);
+        console.log("service Error: ", error);
     }
 } 
+// frontend services dosyası
+const downloadDockerfile = async (props) => {
+  try {
+    const response = await api.get('/file/download-dockerfile', {
+      responseType: 'blob',
+    });
+
+    const blob = new Blob([response.data], { type: 'application/octet-stream' });
+
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'Dockerfile');
+    document.body.appendChild(link);
+
+    link.click();
+    link.parentNode.removeChild(link);
+
+    props.onClose();
+  } catch (error) {
+    console.error('Download error:', error);
+  }
+};
+
+
+
 
 const services = {
     createDockerfile,
+    downloadDockerfile,
 }
 
 export default services;
