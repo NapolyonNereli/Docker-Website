72cc5d5dcfdf081e52b51d3c15ad08cc909d786d
pc <pc@pop-os.localdomain>
2023-09-06 03:22:56 +0300

    Created dockerfile for services

--- backend/src/services/createDockerfile.js
+++ backend/src/services/createDockerfile.js
@@ -1,54 +1,56 @@
-const fs = require('fs');
-// working post method
-exports.createDockerfile = async (from, workdir, copy, run, cmd, expose)  => {
-    const text = `
-        ${from}
-        ${workdir}
-        ${copy}
-        ${run}
-        ${cmd}
-        ${expose}
-    `
-    try {
-        fs.writeFile('Dockerfile.txt', text, (error) => {
-            if(error) {
-                console.log(error);
-                res.status(500).send("Not created file");
-            } else {
-                console.log("Dosya oluşturuldu");
-                res.status(200).send("Dosya oluşturuldu ve yazıldı");
-            }
-        })
-    } catch (error) {
-        console.log("createDockerfile service Error: ", error)
-    }
-}
+const fs = require('fs').promises;
+const path = require('path');
+
+
+exports.createDockerfile = async (from, workdir, run, copy, expose, cmd) => {
+  const dockerfilePath = path.join(__dirname, "../Dockerfile");
+
+  try {
+    const content = `
+FROM ${from}
+WORKDIR ${workdir}
+RUN ${run}
+COPY ${copy}
+EXPOSE ${expose}
+CMD ${cmd}
+`;
+
+    await fs.writeFile(dockerfilePath, content, "utf-8");
+
+    console.log("Dockerfile successfully updated.");
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+};
+
+// services/createDockerfile.js
+
+exports.downloadDockerfile = async () => {
+  const filePath = path.join(__dirname, '../Dockerfile');
+
+  try {
+    const fileStream = await fs.readFile(filePath);
+    return fileStream;
+  } catch (error) {
+    throw error; // Hata fırlatmayı unutmayın
+  }
+};
 
 
 
-exports.downloadDockerfile = (res) => {
-    try {
-      const fileName = 'Dockerfile.txt';
-  
-      // Check File
-      fs.access(fileName, fs.constants.F_OK, (err) => {
-        if (err) {
-          console.error('File not found:', err);
-          res.status(404).send('File not found');
-        } else {
-          // Download file
-          res.download(fileName, (err) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send('Download error', err);
-            } else {
-              console.log('Download successful');
-            }
-          });
-        }
-      });
-    } catch (error) {
-      console.error('Download error:', error);
-      res.status(500).send('Download error');
-    }
-  };
+// exports.downloadDockerfile = () => {
+//   return new Promise((resolve, reject) => {
+//     const filePath = path.join(__dirname, "../Dockerfile");
+
+
+//     fs.readFile(filePath, "utf8", (err, data) => {
+//       if (err) {
+//         console.error(`read Dockerfile error: `, err);
+//         reject({ error: "Error reading Dockerfile" });
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
