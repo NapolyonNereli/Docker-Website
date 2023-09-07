825a8a5f596ccc998e3e2722fdd7bd8781bccd3d
pc <pc@pop-os.localdomain>
2023-09-07 03:42:21 +0300

    Created superUser.js services

---
+++ backend/src/services/superUser.js
@@ -0,0 +1,15 @@
+const {exec} = require('child_process');
+
+
+exports.superUser = async (pass) => {
+    const command = `echo ${pass} | sudo -S docker images`;
+
+    exec(command, (error, stdout) => {
+        if(error) {
+            console.log(error);
+        }else {
+            console.log(stdout);
+            return stdout
+        }
+    })
+}
