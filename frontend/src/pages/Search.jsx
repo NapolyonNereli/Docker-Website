269a4520e966a4a58509d7c2f73e3b8428269d8b
pc <pc@pop-os.localdomain>
2023-09-08 02:35:48 +0300

    Converted data in string format to array

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
+import SearchBar from "../components/SearchBar";
+import ImagesList from "../components/ImagesList";
+import services from "../services/searchImages";
 function Search() {
-  
-  const submitSearch = () => {}
+  const [search, setSearch] = useState("");
+  const [list, setList] = useState([]);
+  const [open, setOpen] = useState(false);
+  // const [download, setDownload] = useState(false);
+
+  const submitSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await services.searchImages(search);
+
+      const data = response.split("\n").map((line) => {
+        const [name, description, stars, official, automated] =
+          line.split(/\s{2,}/);
+        return {
+          name,
+          description,
+          stars,
+          official: official === "[OK]",
+          automated: automated === "[OK]",
+        };
+      });
+      setList(data);
+    } catch (error) {
+      console.error("Hata oluştu: ", error);
+    }
+  };
 
   return (
     <>
       <NavBar />
-      <div className="sm:ml-64">
-        <div className="mt-14">
-          <form className="flex" onSubmit={submitSearch}>
-            <input
-              // value={search}
-              //onChange={(e) => setSearch(e.target.value)}
-              placeholder="Search Image"
-              className="duration-1000 bg-gray-50 w-full text-grey-800 focus:duration-1000 focus:bg-white outline-none focus:outline-none p-2 text-xl lg:text-2xl"
-            />
-            <button
-              type="submit"
-              className="py-2 px-4 hover:duration-500 duration-500 bg-gray-400 hover:bg-gray-500 rounded text-white"
-            >
-              Search
-            </button>
-          </form>
-        </div>
-        </div>
+      <SearchBar submit={submitSearch} search={search} setsearch={setSearch} />
       <SideBar>
-      
+        <ImagesList list={list} />
       </SideBar>
     </>
   );
