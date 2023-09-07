bd09dc1428f098a55d9ddddc6c9671ed2c6d1456
pc <pc@pop-os.localdomain>
2023-09-07 03:26:04 +0300

    Updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const cors = require('cors');
-const containers = require('./routes/containers');
-const file = require('./routes/files')
-
+const cors = require("cors");
+const containers = require("./routes/containers");
+const file = require("./routes/files");
+const root = require("./routes/superUser");
 const app = express();
-const PORT = 3001
+const PORT = 3001;
 
 app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/containers', containers);
-app.use('/file', file);
+app.use("/containers", containers);
+app.use("/file", file);
+app.use("/root", root);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
