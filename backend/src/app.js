b4ab14325b33b0d6a32570c2a6f0ee4d596fe94c
pc <pc@pop-os.localdomain>
2023-09-08 02:30:03 +0300

    Updated app.js backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const containers = require("./routes/containers");
 const file = require("./routes/files");
 const root = require("./routes/superUser");
+const images = require("./routes/imageSearch");
 const app = express();
 const PORT = 3001;
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use("/containers", containers);
 app.use("/file", file);
 app.use("/root", root);
+app.use("/search", images);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
