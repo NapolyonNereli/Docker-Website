308bbaa3b65e60612d4da9f33a3dbbcb5a61f1bd
NapolyonNereli <dev.anise@outlook.com>
2023-09-10 01:02:22 +0300

    Create image install controller function

--- backend/src/controllers/imageSearch.js
+++ backend/src/controllers/imageSearch.js
@@ -1,5 +1,6 @@
 const searchImageService = require('../services/imageSearch');
 
+
 exports.imageSearch = async (req, res) => {
     const {search} = req.body;
 
@@ -7,6 +8,18 @@ exports.imageSearch = async (req, res) => {
         const imageSearch = await searchImageService.imageSearch(search)
         console.log(imageSearch);
         res.send(imageSearch);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.downloadImage = async (req, res) => {
+    const {image} = req.body;
+    
+    try {
+        const downloadImage = await searchImageService.downloadImage(image);
+        console.log(downloadImage);
+        res.send(downloadImage);
     } catch (error) {
         console.log(error);
     }
