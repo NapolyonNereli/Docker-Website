749767db1923061eff31ca619dfe1909ff31be61
NapolyonNereli <dev.anise@outlook.com>
2023-09-10 01:02:56 +0300

    Download routes endpoint

--- backend/src/routes/imageSearch.js
+++ backend/src/routes/imageSearch.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const searchImageController = require('../controllers/imageSearch');
 
 router.post('/images', searchImageController.imageSearch);
+router.post('/download', searchImageController.downloadImage);
 
 module.exports = router;
