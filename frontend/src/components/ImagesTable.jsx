f5a59e8aa7fe96748a59b58b4fafbf88648b26bf
NapolyonNereli <dev.anise@outlook.com>
2023-09-10 19:56:30 +0300

    Added table content, and getting images list element

--- frontend/src/components/ImagesTable.jsx
+++ frontend/src/components/ImagesTable.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React from "react";
 
-function ImagesTable() {
+function ImagesTable({ list }) {
   return (
-    <div className='relative overflow-x-auto'>
-        <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-            <tr>
-                <th scope="col" className="px-6 py-3">REPOSITORY</th>
-                <th scope="col" className="px-6 py-3">TAG</th>
-                <th scope="col" className="px-6 py-3">SIZE</th>
-                <th scope="col" className="px-6 py-3">IMAGE ID</th>
+    <div className="rounded-lg relative overflow-x-auto">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              REPOSITORY
+            </th>
+            <th scope="col" className="px-6 py-3">
+              TAG
+            </th>
+            <th scope="col" className="px-6 py-3">
+              SIZE
+            </th>
+            <th scope="col" className="px-6 py-3">
+              IMAGE ID
+            </th>
+            <th scope="col" className="px-6 py-3">
+                ACTIONS
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((image, index) => (
+            <tr key={index} className={`bg-white border-b ${index % 2 == 0 ? "dark:bg-gray-800" : "dark:bg-gray-700"} dark:border-gray-700`}>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {image.containerId}
+              </th>
+              <td className="px-6 py-4">{image.tag}</td>
+              <td className="px-6 py-4">{image.size}</td>
+              <td className="px-6 py-4">{image.imageID}</td>
+              <td className="px-6 py-4"><button className="px-4 rounded-md text-white py-2 bg-green-600">Start</button></td>
             </tr>
-            </thead>
-            <tbody>
-                <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                </tr>
-            </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default ImagesTable
+export default ImagesTable;
