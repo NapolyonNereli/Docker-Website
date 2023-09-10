da0902827491649664e272959d89a7e942ff9320
NapolyonNereli <dev.anise@outlook.com>
2023-09-11 01:33:23 +0300

    Created createContainer service function

--- backend/src/services/images.js
+++ backend/src/services/images.js
@@ -1,15 +1,33 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
+const { stdout } = require("process");
 
 exports.getImages = async () => {
-    return new Promise((resolve, reject) => {
-        const command = `sudo docker images`;
-        exec(command, (error, stdout) => {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                resolve(stdout);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const command = `sudo docker images`;
+    exec(command, (error, stdout) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
     });
+  });
+};
+
+// Click image button.
+
+exports.createContainer = async (imageID) => {
+  return new Promise((resolve, reject) => {
+    const command = `sudo docker run ${imageID}`;
+
+    exec(command, (error, stdout) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
 };
