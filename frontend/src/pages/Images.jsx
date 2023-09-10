eab608e93815554bb05cb425c4a3e14b80ed1035
NapolyonNereli <dev.anise@outlook.com>
2023-09-10 03:08:45 +0300

    Created Images.jsx page

--- frontend/src/pages/Images.jsx
+++ frontend/src/pages/Images.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar';
 import SideBar from '../components/SideBar';
+import ImagesTable from '../components/ImagesTable';
+import services from '../services/images';
 function Images() {
+  const fetchImages = () => {
+    services.getImages().then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }    
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
   return (
     <>
       <NavBar />
       <SideBar>
-        Images
+        <ImagesTable />
       </SideBar>
     </>
   )
