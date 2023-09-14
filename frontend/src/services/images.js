176977b20f6dfb3e7bfcbbee760150697b730e7e
NapolyonNereli <dev.anise@outlook.com>
2023-09-14 18:14:38 -0400

    deletedImage and runImage api function

--- frontend/src/services/images.js
+++ frontend/src/services/images.js
@@ -1,21 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3001",
 });
 
-
 const getImages = async () => {
-try {
-    const response = await api.get('/images/get-images');
-    return response
-} catch (error) {
+  try {
+    const response = await api.get("/images/get-images");
+    return response;
+  } catch (error) {
     console.log(error);
-}
-}
+  }
+};
+
+const deletedImage = async () => {
+  try {
+    const response = await api.post("/images/delete-image");
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const runImage = async () => {
+  try {
+    const response = await api.post("/images/run-image");
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const services = {
-    getImages,
-}
+  getImages,
+  deletedImage,
+  runImage,
+};
 
 export default services;
