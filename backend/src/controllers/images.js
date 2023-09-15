1d1bec2aae41551230db3717f72c85637f81d716
NapolyonNereli <dev.anise@outlook.com>
2023-09-14 22:28:01 -0400

    Added delete and run images

--- backend/src/controllers/images.js
+++ backend/src/controllers/images.js
@@ -1,11 +1,34 @@
 const imageService = require("../services/images");
 
 exports.getImages = async (req, res) => {
-    try {
-        const images = await imageService.getImages();
-        res.send(images);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("error: ", error); 
-    }
+  try {
+    const images = await imageService.getImages();
+    res.send(images);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error: ", error);
+  }
+};
+
+exports.runContainer = async (req, res) => {
+  const { imageID } = req.body;
+  try {
+    const images = await imageService.createContainer(imageID);
+    res.send(images);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error: ", error);
+  }
+};
+
+exports.deleteImage = async (req, res) => {
+  const { imageID } = req.body;
+
+  try {
+    const images = await imageService.deleteImage(imageID);
+    res.send(images);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error: ", error);
+  }
 };
