28565b4449cc6cbb25448b1990bd06a186d3f82d
NapolyonNereli <dev.anise@outlook.com>
2023-09-16 05:22:18 -0400

    Updated images.js routes file

--- backend/src/controllers/images.js
+++ backend/src/controllers/images.js
@@ -10,23 +10,23 @@ exports.getImages = async (req, res) => {
   }
 };
 
-exports.runContainer = async (req, res) => {
-  const { imageID } = req.body;
-  try {
-    const images = await imageService.createContainer(imageID);
-    res.send(images);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("error: ", error);
-  }
-};
+// exports.runContainer = async (req, res) => {
+//   const { imageID } = req.body;
+//   try {
+//     const images = await imageService.createContainer(imageID);
+//     res.send(images);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send("error: ", error);
+//   }
+// };
 
 exports.deleteImage = async (req, res) => {
   const { imageID } = req.body;
 
   try {
-    const images = await imageService.deleteImage(imageID);
-    res.send(images);
+    const deleteImage = await imageService.deleteImage(imageID);
+    res.status(200).send(deleteImage);
   } catch (error) {
     console.log(error);
     res.status(500).send("error: ", error);
