2f2d6470d2f15bfe7a09f74c371471cf441c2fa8
NapolyonNereli <dev.anise@outlook.com>
2023-09-16 05:22:57 -0400

    Remove run image routes

--- backend/src/routes/images.js
+++ backend/src/routes/images.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const getImagesController = require("../controllers/images");
 
 router.get("/get-images", getImagesController.getImages);
-router.post("/run-container", getImagesController.runContainer);
 router.post("/delete-image", getImagesController.deleteImage);
 
 module.exports = router;
