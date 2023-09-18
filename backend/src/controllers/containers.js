52a7c5bfe64536c5c4b05dfc37e0263c2325ef8c
NapolyonNereli <dev.anise@outlook.com>
2023-09-18 04:46:34 -0400

    Updatted containers controller page

--- backend/src/controllers/containers.js
+++ backend/src/controllers/containers.js
@@ -1,12 +1,10 @@
 const containerService = require("../services/containers");
 
-
 exports.listContainer = async (req, res) => {
-    const {veri} = req.body;
-    try {
-        const listContainer = await containerService.listContainers(veri);
-        res.status(200).send(listContainer)
-    } catch (error) {
-        console.log("error controller container :",error)
-    }
-}
+  try {
+    const listContainer = await containerService.listContainers();
+    res.status(200).send(listContainer);
+  } catch (error) {
+    console.log("error controller container :", error);
+  }
+};
