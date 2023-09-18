00b66fe49b727e0f3a9cf212b1a075b704a399e1
NapolyonNereli <dev.anise@outlook.com>
2023-09-18 04:47:22 -0400

    Deleted post router added get and updated containers.js

--- backend/src/routes/containers.js
+++ backend/src/routes/containers.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const containersController = require('../controllers/containers');
+const containersController = require("../controllers/containers");
 
-router.post('/get-containers', containersController.listContainer);
+router.get("/get-containers", containersController.listContainer);
 
 module.exports = router;
