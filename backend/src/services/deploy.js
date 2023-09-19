2e490d5b0fc430c4174e162fa0235aeda4d3ee23
NapolyonNereli <dev.anise@outlook.com>
2023-09-19 05:27:13 -0400

    Updated deploy.js in service

--- backend/src/services/deploy.js
+++ backend/src/services/deploy.js
@@ -1,11 +1,12 @@
 const { exec } = require("child_process");
 
-exports.buildImage = async (path, name) => {
+exports.buildImage = async (text, name) => {
   return new Promise((resolve, reject) => {
-    const command = `sudo docker build --tag ${name} ${path}`;
+    const command = `sudo docker build -t ${name} ${text}`;
 
     exec(command, (error, stdout) => {
       if (error) {
+        console.log("text:", text);
         reject(error);
       } else {
         resolve(stdout);
