cdce64974014f29e711570b4b1db27e1e52b5468
NapolyonNereli <dev.anise@outlook.com>
2023-09-19 05:23:59 -0400

    Created deploy controllers updated

--- backend/src/controllers/deploy.js
+++ backend/src/controllers/deploy.js
@@ -1,11 +1,11 @@
-const deployBuild = require('../services/deploy');
+const deployBuild = require("../services/deploy");
 
 exports.buildImage = async (req, res) => {
-    const {name, path} = req.body;
-    try {
-        const buildImage = await deployBuild.buildImage(name, path);
-        res.status(200).send(buildImage);
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const { text, name } = req.body;
+  try {
+    const buildImage = await deployBuild.buildImage(text, name);
+    res.status(200).send(buildImage);
+  } catch (error) {
+    console.log(error);
+  }
+};
