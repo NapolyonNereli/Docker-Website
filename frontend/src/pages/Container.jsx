8c9a86230c17c842305c9f8311630a1fa9594851
NapolyonNereli <dev.anise@outlook.com>
2023-09-19 05:40:33 -0400

    Showing the captured APIs on the screen

--- frontend/src/pages/Container.jsx
+++ frontend/src/pages/Container.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import SideBar from '../components/SideBar'
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
+import ContainerTable from "../components/ContainerTable";
+import services from "../services/containers";
 
 function Container() {
+  const [containers, setContainers] = useState([]);
+
+  const fetchImages = () => {
+    services
+      .getContainers()
+      .then((response) => {
+        const containerLines = response.data.split("\n");
+
+        containerLines.shift();
+        containerLines.pop();
+
+        const containersData = containerLines.map((line) => {
+          const [containerId, image, command, created, status, port, names] =
+            line.split(/\s+/);
+          return { containerId, image, command, created, status, port, names };
+        });
+
+        setContainers(containersData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <>
       <NavBar />
-    <SideBar >
-
-       //containers table
-      Container 
-      
-
-    </SideBar>
+      <SideBar>
+        <ContainerTable list={containers} />
+      </SideBar>
     </>
-  )
+  );
 }
 
-export default Container
+export default Container;
