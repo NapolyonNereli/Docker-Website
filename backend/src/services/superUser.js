dfdb75758598913309eba2dffc765bc9a391c3ad
NapolyonNereli <dev.anise@outlook.com>
2023-09-22 06:12:44 -0400

    Updated command variable

--- backend/src/services/superUser.js
+++ backend/src/services/superUser.js
@@ -1,15 +1,14 @@
-const {exec} = require('child_process');
-
+const { exec } = require("child_process");
 
 exports.superUser = async (pass) => {
-    const command = `echo ${pass} | sudo -S docker images`;
+  const command = `echo ${pass} | sudo -S -v && sudo docker images`;
 
-    exec(command, (error, stdout) => {
-        if(error) {
-            console.log(error);
-        }else {
-            console.log("superUser: ", true);
-            return stdout
-        }
-    })
-}
+  exec(command, (error, stdout) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("superUser: ", true);
+      return stdout;
+    }
+  });
+};
