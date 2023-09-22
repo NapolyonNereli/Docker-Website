0ad97dec62d290131d95cdd9cd991fcc08df326a
NapolyonNereli <dev.anise@outlook.com>
2023-09-22 06:10:54 -0400

    Added stop-cointer router

--- backend/src/routes/containers.js
+++ backend/src/routes/containers.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const containersController = require("../controllers/containers");
 
 router.get("/get-containers", containersController.listContainer);
+router.post("/stop-container", containersController.stopContainer);
 
 module.exports = router;
