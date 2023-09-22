d00f2cfaac5e8c6e8c3a6c423714aee21070a588
NapolyonNereli <dev.anise@outlook.com>
2023-09-22 04:42:30 -0400

    Added stopContainer controller

--- backend/src/controllers/containers.js
+++ backend/src/controllers/containers.js
@@ -8,3 +8,15 @@ exports.listContainer = async (req, res) => {
     console.log("error controller container :", error);
   }
 };
+
+exports.stopContainer = async (req, res) => {
+  const { imageID } = req.body;
+
+  try {
+    const stopContainer = await containerService.stopContainer(imageID);
+    res.status(200).send(stopContainer);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error: ", error);
+  }
+};
