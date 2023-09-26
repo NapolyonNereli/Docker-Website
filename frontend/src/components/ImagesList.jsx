bb1388ca8645a0160349c37af6c3e6acef0ba4a2
NapolyonNereli <dev.anise@outlook.com>
2023-09-26 18:31:53 -0400

    Grid property of list elements was set to two. Gap-4 added to classname

--- frontend/src/components/ImagesList.jsx
+++ frontend/src/components/ImagesList.jsx
@@ -42,7 +42,7 @@ function ImagesList({ list }) {
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 opacity-50 z-50"></div>
       )}
 
-      <div className="grid grid-cols-1">
+      <div className="grid grid-cols-2 gap-4">
         {list.map((item, index) => (
           <div
             key={index}
