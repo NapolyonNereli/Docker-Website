e7985f8d74cea3f05cc3e24930499d7a4b9e0a5f
NapolyonNereli <dev.anise@outlook.com>
2023-09-25 20:11:51 -0400

    Updated page

--- frontend/src/pages/Images.jsx
+++ frontend/src/pages/Images.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import NavBar from '../components/NavBar';
-import SideBar from '../components/SideBar';
-import ImagesTable from '../components/ImagesTable';
-import services from '../services/images';
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
+import ImagesTable from "../components/ImagesTable";
+import services from "../services/images";
 function Images() {
-
-  const [images, setImages] = useState([]); 
+  const [images, setImages] = useState([]);
 
   const fetchImages = () => {
-    services.getImages().then((response) => {
-      const containerLines = response.data.split("\n");
+    services
+      .getImages()
+      .then((response) => {
+        const containerLines = response.data.split("\n");
 
-      containerLines.shift();
-      containerLines.pop();
-      
-      const imagesData = containerLines.map((line) => {
-        const [name, tag, imageID, created, time, ago, size] = line.split(/\s+/);
-        return { name, tag, imageID, created, time, ago, size };
+        containerLines.shift();
+        containerLines.pop();
+
+        const imagesData = containerLines.map((line) => {
+          const [name, tag, imageID, created, time, ago, size] =
+            line.split(/\s+/);
+          return { name, tag, imageID, created, time, ago, size };
+        });
+
+        setImages(imagesData);
       })
-
-      setImages(imagesData)
-
-    }).catch((error) => {
-      console.log(error);
-    })
-  }    
-
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   useEffect(() => {
     fetchImages();
-  }, [])
+  }, []);
 
   return (
     <>
@@ -37,7 +38,7 @@ function Images() {
         <ImagesTable list={images} />
       </SideBar>
     </>
-  )
+  );
 }
 
-export default Images
+export default Images;
