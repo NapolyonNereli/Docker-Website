48a56ddf849a60043955828edd5bece0879067fb
NapolyonNereli <dev.anise@outlook.com>
2023-09-28 15:10:01 -0400

    Added selected props

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div>
       <NavBar />
-      <SideBar>
+      <SideBar selected = "home">
         <section className="mt-24 ">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
