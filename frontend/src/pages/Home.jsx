9e581688b6e7b385a9699b9fcef68aa065d5ee50
pc <pc@pop-os.localdomain>
2023-10-03 18:55:28 -0400

    Added redirect to docker's page to download docker

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,18 +3,18 @@ import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 
 function Home() {
+
   return (
     <div>
       <NavBar />
-      <SideBar selected = "home">
+      <SideBar selected="home">
         <section className="mt-24 ">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
               We invest in the world’s potential
             </h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-              Here at Flowbite we focus on markets where technology, innovation,
-              and capital can unlock long-term value and drive economic growth.
+              Please download Docker before using the application.
             </p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
               <a
@@ -22,6 +22,9 @@ function Home() {
                 className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-black hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               >
                 Learn more
+              </a>
+              <a target="_blank" href="https://docs.docker.com/engine/install/ubuntu/" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-black hover:text-white dark:border-green-700 dark:hover:bg-green-700 dark:focus:ring-green-400">
+                Download Docker
               </a>
             </div>
           </div>
