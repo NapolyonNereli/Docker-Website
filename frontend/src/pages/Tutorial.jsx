dd730552d8715f541b2c5122935093824929233f
pc <pc@pop-os.localdomain>
2023-10-08 15:25:14 -0400

    Updated page

--- frontend/src/pages/Tutorial.jsx
+++ frontend/src/pages/Tutorial.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Tutorial() {
   return (
-    <div className='text-center'>
-        asd
-    </div>
+    <>
+    <aside
+        className="fixed top-0 left-0 z-40 w-52 h-screen pt-20 transition-transform  bg-white border-r border-gray-200 sm:translate-x-0 "
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white ">
+        <ul className="space-y-2 ">
+        <li className=' hover:underline'>
+          <a href=''>Main</a>
+        </li>
+        <li className=' hover:underline'>
+          <a href="">How to use app</a>
+        </li>
+        <li className=' hover:underline'>
+          <a href=''>Create Dockerfile</a>
+        </li>
+        <li className=' hover:underline'>
+          <a href='' className='text-sm'>Search & download image</a>
+        </li>
+        <li className=' hover:underline'></li>
+        </ul>
+        </div>
+      </aside>
+      <div className=" p-4 sm:ml-64">
+        <div>
+          <h1 className='text-center'>Main</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
